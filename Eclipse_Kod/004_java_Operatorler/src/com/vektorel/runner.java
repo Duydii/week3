0c456630d3405bc28c5d78e019f4622aff0dd2bf
Duydii <96373374+Duydii@users.noreply.github.com>
2022-01-08 13:18:28 +0300

    Add files via upload

---
+++ Eclipse_Kod/004_java_Operatorler/src/com/vektorel/runner.java
@@ -0,0 +1,12 @@
+package com.vektorel;
+
+public class runner {
+
+	public static void main(String[] args) {
+		// Operatörler 
+		// 1. Aritmetik operatörler
+		// 2. Görev Operatörleri
+		// 3. Ýliþkisel operatörler
+	}
+
+}
